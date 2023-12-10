4909f7a583f1c5761e78cb81ccd27b9360f282a3
kuk329 <kuk329@cau.ac.kr>
2023-12-10 17:20:41 +0900

    test(lotto): 당첨번호 저장을 위한 WinningNumbers 클래스에 대한 단위 테스트 작성

---
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersTest {
+
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 6개만 입력 가능합니다.");
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복되는 번호가 있습니다.");
+    }
+
+
+    @DisplayName("당첨 번호에 로또 번호 범위를 넘어선 숫자가 입력되면 예외가 발생한다..")
+    @Test
+    void createWinningNumberByOverNumber() {
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+
+
+}
